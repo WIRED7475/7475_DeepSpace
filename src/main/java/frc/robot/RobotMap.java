3ac30a9578c9c4ded4ce84b0d07474dff05fb7dd
WIRED 7475 <conhugmodder@gmail.com>
2019-03-16 20:03:21 -0400

    Adding comments to RobotMap

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -4,16 +4,20 @@ package frc.robot;
 
 public class RobotMap 
 {
+    //Drive Motors
     static public int blMotor_num = 1;
     static public int flMotor_num = 0;
     static public int brMotor_num = 2;
     static public int frMotor_num = 3;
 
+    //Reel Motors
     public static int leftReel_num = 6;
     public static int rightReel_num = 7;
- 
+    
+    //Intake Motors
     public static int leftIntake_num = 4;
     public static int rightIntake_num = 5;
 
+    //Wrist Motors
     public static int wristMotor_num = 8;
 }
